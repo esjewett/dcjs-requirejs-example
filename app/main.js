30c21a609ae8915e1a25810dee39fcb2e0e937cf
Ethan Jewett <esjewett@gmail.com>
2017-05-19 11:38:28 -0500

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,28 @@
+define(['dc', 'crossfilter'], function (dc, crossfilter) {
+
+    console.log(dc)
+    
+    var chart = dc.barChart("#dc-chart");
+    d3.csv("morley.csv", function(error, experiments) {
+        experiments.forEach(function(x) {
+            x.Speed = +x.Speed;
+        });
+        var ndx                 = crossfilter(experiments),
+            runDimension        = ndx.dimension(function(d) {return +d.Run;}),
+            speedSumGroup       = runDimension.group().reduceSum(function(d) {return d.Speed * d.Run / 1000;});
+        chart
+            .width(768)
+            .height(480)
+            .x(d3.scale.linear().domain([6,20]))
+            .brushOn(false)
+            .yAxisLabel("This is the Y Axis!")
+            .dimension(runDimension)
+            .group(speedSumGroup)
+            .on('renderlet', function(chart) {
+                chart.selectAll('rect').on("click", function(d) {
+                    console.log("click!", d);
+                });
+            });
+            chart.render();
+    });
+})
